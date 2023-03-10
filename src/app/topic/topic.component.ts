ec1357cc9017873932d7cba368528a5609b684f0
solomonsanthosh <solosanty2064@gmail.com>
2023-03-10 08:09:55 +0530

    SS

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -10,6 +10,47 @@ import { Router } from '@angular/router';
 export class TopicComponent implements OnInit {
   selected: any = null;
   user: any;
+  topicIndex: number = 0;
+  topics = [
+    {
+      topic: 'stress',
+      question1: 'Do you feel Overwhelmed ?',
+      question2: 'Do you feel Lonely or Isolated ?',
+      question3: 'Do you feel Tired ?',
+      question4: 'Do you feel Discouraged ?',
+      question5: 'Do you feel loaded down with responsibility ?',
+    },
+    {
+      topic: 'depression',
+      question1: 'Feeling down, depressed, or hopeless ?',
+      question2: 'Trouble falling or staying asleep, or sleeping too much ?',
+      question3: 'Feeling tired or having little energy ?',
+      question4: 'Trouble concentrating on things ?',
+      question5:
+        'Moving or speaking so slowly that other people could have noticed ?',
+    },
+    {
+      topic: 'anxiety',
+      question1: 'Feeling nervous, anxious, or on edge ?',
+      question2: 'Not being able to stop or control worrying ?',
+      question3: 'Worrying too much about different things ?',
+      question4: 'Feeling afraid, as if something awful might happen ?',
+      question5: 'Becoming easily annoyed or irritable ?',
+    },
+    {
+      topic: 'ptsd',
+      question1:
+        'had nightmares about the events or thought about the events when you did not want to ?',
+      question2: 'been constantly on guard, watchful, or easily startled ?',
+      question3:
+        'felt numb or detached from people, activities, or your surroundings ?',
+      question4:
+        'felt guilty or unable to stop blaming yourself or others for the events or any problems the events may have caused ?',
+      question5:
+        'tried hard not to think about the events or went out of your way to avoid situations that reminded you of the events ?',
+    },
+  ];
+
   constructor(
     private nav: NavService,
     private http: HttpClient,
@@ -22,11 +63,19 @@ export class TopicComponent implements OnInit {
   onTopicSelect = (data: any) => {
     this.selected = data;
   };
+  changeTopic() {
+    if (this.topicIndex == 3) {
+      this.topicIndex = 0;
+      this.save();
+    } else {
+      this.topicIndex = this.topicIndex + 1;
+    }
+  }
   save() {
     this.http
       .post<any>('http://18.181.218.216:8000/api/topic', {
         user: {
-          topic: this.selected,
+          topic: this.topics[this.topicIndex].topic,
           email: this.user.email,
         },
       })
